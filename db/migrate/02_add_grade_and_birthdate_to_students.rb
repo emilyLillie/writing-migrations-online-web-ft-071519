4c8f8c66505190761b51d1bc9b841cba2c5feb1e
Emily Lillie <emilylillie3@gmail.com>
2019-08-20 21:52:09 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,6 +2,7 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
   
   def change 
     add_column :students, :grade, :integer 
+    add_column :students, :birthdate, :string 
     
   end 
   
